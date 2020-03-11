022ed6a2d317045c1c3835b7948cf8c2245ea4b4
Gabryel Ryba <gabryel.ryba@gmail.com>
2020-03-11 18:25:55 -0300

    estilização e login button

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ $(function(){
       dateFormat: "dd-mm-yy"
   });
 });
+
+const emailInput = document.getElementById('user-email-phone');
+const loginBtn = document.getElementById('button-login');
+loginBtn.addEventListener('click', LoginAlert);
+
+function LoginAlert() {
+  alert(emailInput.value);
+}
