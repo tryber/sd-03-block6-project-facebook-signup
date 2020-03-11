76dac9cb26566546438461b858dced11d83f29f4
Thiago Tavares <thiagooo15@gmail.com>
2020-03-11 18:24:01 -0300

    inclementando js

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ const titulo = document.getElementsByClassName ('h1')[0];
 function mudabgCor(){
     titulo.style.Color = 'red'
 }
+const input = document.getElementById('user-email-phone');
+
+window.onload = function () {
+	const submitLogin = document.getElementById('button-login');	
+	const submitForm = document.getElementById('facebook-register');
+
+	submitLogin.addEventListener('click', submit);
+	submitForm.addEventListener('click', verificaFormulario);
+}
+
+// login
+// Formulario
+// const nomeForm = document.getElementById('nomeForm');
+// const sobreNomeForm = document.getElementById('sobrenomeForm');
+// const emailForm = document.getElementById('emailForm');
+// const senhaForm = document.getElementById('senhaForm');
+// const nascimentoForm = document.getElementById('senhaForm');
+// const radioFemForm = document.getElementById('femininoForm');
+// const radioMascForm = document.getElementById('masculinoForm');
+// const radioPersonForm = document.getElementById('personForm');
+// submit
+
+function submit() {
+	alert(input.value);
+}
+
+// function verificaFormulario() {
+// }
