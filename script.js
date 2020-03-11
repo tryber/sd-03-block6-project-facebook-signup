80a804f37d703059872412f5103422a6f8b237a5
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-11 19:53:01 -0300

    updates

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-window.onload= function(){
-    let botaoEmailTelefone= document.getElementById('button-login')
-    botaoEmailTelefone.addEventListener('click', function(evento){
-        let emailTelefone= document.getElementById('user-email-phone')
-
-            window.alert(`Olá, ${emailTelefone.value}`)
+window.onload = function(){
+    let botaoEmailTelefone = document.getElementById ('button-login')
+    botaoEmailTelefone.addEventListener ('click', function(evento){
+        let emailTelefone = document.getElementById ('user-email-phone')
+        window.alert(`Olá, ${emailTelefone.value}`)
     })
-
-    //let novaNome= document.getElementById('nome')
-    //let novaSobrenome= document.getElementById('sobrenome')
-    //let novaCelEmail= document.getElementById('celularEmail')
-    //let novaSenha= document.getElementById('novaSenha')
-    //let novaData= document.getElementById('data')
-    //let novaGenF= document.getElementById('Feminino')
-    //let novaGenM= document.getElementById('Masculino')
-    //let novaPers= document.getElementById('Personalizado')
-    //let botaoCadasatrar= document.getElementById('facebook-register')
-    //botaoCadasatrar.addEventListener('click', function(){
-    //    if(.value|| ||)
-    //})
 }
