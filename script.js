6d116faa6d15b80e1b17b2a71819ee18dabf9f8e
leonardodamasceno8 <leonardodamasceno8@users>
2020-03-11 17:50:27 -0300

    todos requisitos em HTML

---
+++ script.js
@@ -0,0 +1,31 @@
+//login
+const submitLogin = document.getElementById('button-login');
+const input = document.getElementById('user-email-phone');
+
+//Formulario
+const nomeForm = document.getElementById('nomeForm');
+const sobreNomeForm = document.getElementById('sobrenomeForm');
+const emailForm = document.getElementById('emailForm');
+const senhaForm = document.getElementById('senhaForm');
+const nascimentoForm = document.getElementById('senhaForm');
+const radioFemForm = document.getElementById('femininoForm');
+const radioMascForm = document.getElementById('masculinoForm');
+const radioPersonForm = document.getElementById('personForm');
+//submit
+const submitForm = document.getElementById('facebook-register');
+
+
+// console.log(input.value)
+
+window.onload = function () {
+    submitLogin.addEventListener ('click', submit)
+    submitForm.addEventListener ('click', verificaFormulario)
+}
+
+function submit () {
+    alert (input.value)
+}
+
+function verificaFormulario(){
+
+}
