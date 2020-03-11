688974b15df2067d1d4971aeb346d591c9deea93
leonardodamasceno8 <leonardodamasceno8@users>
2020-03-11 18:08:00 -0300

    code climate checked

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-// login    
+// login
 const submitLogin = document.getElementById('button-login');
 const input = document.getElementById('user-email-phone');
 
-//Formulario    
-// const nomeForm = document.getElementById('nomeForm');    
-// const sobreNomeForm = document.getElementById('sobrenomeForm');  
-// const emailForm = document.getElementById('emailForm');  
-// const senhaForm = document.getElementById('senhaForm');  
-// const nascimentoForm = document.getElementById('senhaForm'); 
-// const radioFemForm = document.getElementById('femininoForm');    
-// const radioMascForm = document.getElementById('masculinoForm');  
-// const radioPersonForm = document.getElementById('personForm');   
-//submit        
+// Formulario
+// const nomeForm = document.getElementById('nomeForm');
+// const sobreNomeForm = document.getElementById('sobrenomeForm');
+// const emailForm = document.getElementById('emailForm');
+// const senhaForm = document.getElementById('senhaForm');
+// const nascimentoForm = document.getElementById('senhaForm');
+// const radioFemForm = document.getElementById('femininoForm');
+// const radioMascForm = document.getElementById('masculinoForm');
+// const radioPersonForm = document.getElementById('personForm');
+// submit
 const submitForm = document.getElementById('facebook-register');
 
 window.onload = function () {
-  submitLogin.addEventListener ('click', submit);
-  submitForm.addEventListener ('click', verificaFormulario)
+  submitLogin.addEventListener('click',submit);
+  submitForm.addEventListener('click',verificaFormulario)
 }
 
 function submit () {
-  alert (input.value)
+  alert(input.value)
 }
 
-function verificaFormulario(){
+function verificaFormulario() {
 }
