7e88a5baeb3b9ef8b0cd066c603ca4ea401807c0
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-11 19:30:05 -0300

    codeclimate check 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const botaoLogin = document.querySelector(".button-login");
-botaoLogin.addEventListener("click", function() {
-  const inputLoginValue = document.querySelector(".login");
+const botaoLogin = document.querySelector('.button-login');
+botaoLogin.addEventListener('click', function () {
+  const inputLoginValue = document.querySelector('.login');
   alert(`Seu email ou telefone é: ${inputLoginValue.value}`);
 });
