0cbe16ce8a842e33e9b5162294d1241b42833a4c
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-11 17:32:42 -0300

    algo

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ window.onload= function(){
             window.alert(`Olá, ${emailTelefone.value}`)
         }
     })
+
+    let novaNome= document.getElementById("nome")
+    let novaSobrenome= document.getElementById("sobrenome")
+    let novaCelEmail= document.getElementById("celularEmail")
+    let novaSenha= document.getElementById("novaSenha")
+    let novaData= document.getElementById("data")
+    let novaGenF= document.getElementById("Feminino")
+    let novaGenM= document.getElementById("Masculino")
+    let novaPers= document.getElementById("Personalizado")
+    let botaoCadasatrar= document.getElementById("facebook-register")
+    botaoCadasatrar.addEventListener("click", function(){
+        
+    })
 }
