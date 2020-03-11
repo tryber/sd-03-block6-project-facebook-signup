51e0e4103e1355615a97a2067d5cda368c5326cf
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-11 19:08:21 -0300

    Corrigindo code climate

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ for (let cont = 2020; cont > 1904; cont -= 1) {
   const option = document.createElement('option');
   option.textContent = cont;
   document.getElementById('year').appendChild(option);
-};
+}
 
 for (let cont = 1; cont < 32; cont += 1) {
   const option = document.createElement('option');
   option.textContent = cont;
   document.getElementById('day').appendChild(option);
-};
+}
