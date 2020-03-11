b14e763a291806ac9119d81334801f21008b89cc
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-11 17:28:34 -0300

    Code Climate Issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let datepicker = document.getElementById('datepicker').DatePickerX; 	
-datepicker.init({format: 'dd/mm/yyyy'});
+const datepicker = document.getElementById('datepicker').DatePickerX;
+datepicker.init({ format: 'dd/mm/yyyy' });
 
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
