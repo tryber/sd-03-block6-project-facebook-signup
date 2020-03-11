ce0bfdc890b5dc28bc3bb4ba5c76c2d69b55ec6b
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-11 16:04:44 -0300

    Adcionando DatePickerX

---
+++ script.js
@@ -0,0 +1,6 @@
+const datepicker = document.getElementById('datepicker').DatePickerX; 	
+datepicker.init({format: 'dd/mm/yyyy'});
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+});
