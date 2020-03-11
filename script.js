f2b55e055290d4b0e67bd4089bea5540b7e20326
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-11 18:25:37 -0300

    Code Climate css issues 3

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const datepicker = document.getElementById('datepicker').DatePickerX;
 datepicker.init({ format: 'dd/mm/yyyy' });
 
 document.getElementById('register').addEventListener('submit', (e) => {
-
   e.preventDefault();
   const value = document.getElementById('name').value;
   const gender = document.getElementsByName('gender');
   let checked = '';
-  for (let i = 0; i < gender.length; i+=1) {
-    if(gender[i].checked) {
-	  checked = gender[i].value;
-	}
+  for (let i = 0; i < gender.length; i += 1) {
+    if (gender[i].checked) {
+      checked = gender[i].value;
+	  }
   }
 
   alert(`${value} - ${checked}`);
