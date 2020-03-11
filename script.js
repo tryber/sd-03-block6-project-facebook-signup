f2972a7fe00573e85cfda6c39df95df470e6a5a9
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-11 19:59:28 -0300

    update4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = function(){
-    let botaoEmailTelefone = document.getElementById ('button-login')
-    botaoEmailTelefone.addEventListener ('click', function(evento){
-        let emailTelefone = document.getElementById ('user-email-phone')
+window.onload = function() {
+    let botaoEmailTelefone = document.getElementById('button-login')
+    botaoEmailTelefone.addEventListener('click', function(evento) {
+        let emailTelefone = document.getElementById('user-email-phone')
         window.alert(`Olá, ${emailTelefone.value}`)
     })
 }
