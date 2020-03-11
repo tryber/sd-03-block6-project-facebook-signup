78242cb40783356ef6f4647304743233c7a5707b
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-11 18:49:50 -0300

    Corrigindo code climate

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const month = ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
 
-month.forEach(e => {
-  let option = document.createElement("option");
+month.forEach((e) => {
+  const option = document.createElement('option');
   option.textContent = e;
-  document.getElementById("month").appendChild(option);
+  document.getElementById('month').appendChild(option);
 });
 
-for (let cont = 2020; cont > 1904; cont--) {
-  let option = document.createElement("option");
+for (let cont = 2020; cont > 1904; cont -= 1) {
+  const option = document.createElement('option');
   option.textContent = cont;
-  document.getElementById("year").appendChild(option);
+  document.getElementById('year').appendChild(option);
 };
 
-for (let cont = 1; cont < 32; cont++) {
-  let option = document.createElement("option");
+for (let cont = 1; cont < 32; cont += 1) {
+  const option = document.createElement('option');
   option.textContent = cont;
-  document.getElementById("day").appendChild(option);
+  document.getElementById('day').appendChild(option);
 };
