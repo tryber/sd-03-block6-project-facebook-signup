a0daf04c0cc2b9c61733c50e3a26a3e398b613b2
Hfreitas <hfreitas86@gmail.com>
2020-03-11 18:22:52 -0300

    Adequação de arquivos ao code climate

--- script.js
+++
@@ -1,9 +0,0 @@
-//
-
-const field = document.getElementById('datepicker');
-const picker = new Pikaday({
-  onSelect: function(date) {
-    field.value = picker.toString();
-  },
-});
-field.parentNode.insertBefore(picker.el, field.nextSibling);
