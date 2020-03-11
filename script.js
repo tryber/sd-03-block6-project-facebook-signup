037e36929133ea9a017a87eb1a4888765b34ba25
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-11 17:53:31 -0300

    Juntando com código do Gabriel

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ datepicker.init({ format: 'dd/mm/yyyy' });
 
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
+  let value = document.getElementById('name').value;
+  let gender = document.getElementsByName('gender');
+  let checked = "";
+  for(i = 0; i < gender.length; i++) { 
+	    if(gender[i].checked) {
+	      checked = gender[i].value; 
+	    }
+    }
+
+  alert(value + " - " + checked);
 });
