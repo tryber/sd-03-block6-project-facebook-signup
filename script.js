39154e525642240c43e0416f7cad29e344d42745
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-12 17:00:53 -0300

    Adicionando alert do requisito 7

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-// const month = ['Jan', 'Fev', 'Mar', 'Abr',
-// 'Maio', 'Jun', 'Jul', 'Ago', 'Set',
-// 'Out', 'Nov', 'Dez'];
-
-// month.forEach((e) => {
-//   const option = document.createElement('option');
-//   option.textContent = e;
-//   document.getElementById('month').appendChild(option);
-// });
-
-// for (let cont = 2020; cont > 1904; cont -= 1) {
-//   const option = document.createElement('option');
-//   option.textContent = cont;
-//   document.getElementById('year').appendChild(option);
-// }
-
-// for (let cont = 1; cont < 32; cont += 1) {
-//   const option = document.createElement('option');
-//   option.textContent = cont;
-//   document.getElementById('day').appendChild(option);
-// }
+document.getElementById("button-login").addEventListener("click", function () {
+  console.log(document.getElementById("user-email-phone").value);
+  alert(document.getElementById("user-email-phone").value);
+  document.getElementById("user-email-phone").value = "";
+  document.getElementById("user-password").value = ""
+})
