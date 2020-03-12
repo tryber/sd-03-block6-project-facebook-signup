7b5ee74e77a64a899f9c675c3e295320b6440416
henrique <hbaetaleite@gmail.com>
2020-03-11 23:46:08 -0300

    Corrigindo requisitos 7 e 11

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const botaoLogin = document.getElementById('button-login');
+const inputUser = document.getElementById('user-email-phone');
 
 botaoLogin.addEventListener('click', function () {
-  alert('Olá Henrique!');
+  alert(`Olá ${inputUser.value}! Seja bem-vindo!`);
 });
