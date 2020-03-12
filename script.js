8e4f203c18427f56e7c9fc28361211fe86b32b1d
Clayton Pereira <cpereiramt@gmail.com>
2020-03-12 18:37:42 -0400

    Ajustando os requisitos do projeto

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+
 const buttonlogin = document.getElementById('button-login');
 buttonlogin.addEventListener ('click', function() {
-    let user = document.getElementById('user-email-phone').value;
-  alert('o usuário é ' + ': ' + user);
+  const user = document.getElementById('user-email-phone').value;
+  alert('o usuário é : ' + user);
 });
