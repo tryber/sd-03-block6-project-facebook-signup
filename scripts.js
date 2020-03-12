3921e0f7346cf1e9684444d47391b3dfa98af092
potew <potew0@gmail.com>
2020-03-12 11:08:38 -0300

    Ajuste no script.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-document.getElementById('button-login').addEventListener('click', pseudoLogin);
-
 function pseudoLogin() {
-    alert(document.getElementById('user-email-phone').value);
-};
+  alert(document.getElementById('user-email-phone').value);
+}
+
+document.getElementById('button-login').addEventListener('click', pseudoLogin);
