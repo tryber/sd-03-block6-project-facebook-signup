bb00478376714ec8a122adee27fb11c868066881
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-12 17:39:33 -0300

    Adicinando date picker

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.getElementById('date').DatePickerX.init({ format: "dd/mm/yyyy" });
+
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
   document.getElementById('user-email-phone').value = '';
