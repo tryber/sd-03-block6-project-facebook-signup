2c26fa44cff0cd92c6cbb1737ad3b14649a2593b
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-12 17:04:02 -0300

    Consertando erros do code climate

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-document.getElementById("button-login").addEventListener("click", function () {
-  console.log(document.getElementById("user-email-phone").value);
-  alert(document.getElementById("user-email-phone").value);
-  document.getElementById("user-email-phone").value = "";
-  document.getElementById("user-password").value = ""
-})
+document.getElementById('button-login').addEventListener('click', function () {
+  alert(document.getElementById('user-email-phone').value);
+  document.getElementById('user-email-phone').value = '';
+  document.getElementById('user-password').value = '';
+});
