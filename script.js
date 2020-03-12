6e8ca8c68940a043e1bf71898272cf15b74d0b54
rogerfaustino <roger_faustino@hotmail.com>
2020-03-12 12:23:35 -0300

    Deleting JS code

--- script.js
+++
@@ -1,7 +0,0 @@
-var field = document.getElementById('datepicker');
-var picker = new Pikaday({
-    onSelect: function(date) {
-        field.value = picker.toString();
-    }
-});
-field.parentNode.insertBefore(picker.el, field.nextSibling);
