0069e19e8cc39160632e25afa1a428e3c0c0bbaf
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-12 00:11:49 -0300

    reestruturacao do html

---
+++ script.js
@@ -0,0 +1,25 @@
+function verificaData() {
+  let day = document.getElementById('day')
+  () ? return true : return false;
+}
+
+function criaConta () {
+  preventDefault();
+  let verificador = true;// Se chegar como true no final ta td certo, caso contrario, dados inválidos
+  verificador = verificador * verificaData();
+  if (verificador) {
+
+  } else {
+    window.alert('Dados inválidos');
+  }
+}
+
+function LOADED() {
+  let buttonCadastreSe = document.getElementById('facebook-register');
+  buttonCadastreSe.addEventListener('click', criaConta);
+
+  console.log(picker);
+  
+}
+
+window.onload = LOADED();
