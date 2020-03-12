4690e4be93a63fb1bffcb4c3b108f8dda8008c93
Luma Arruda <luma.oarruda@gmail.com>
2020-03-12 19:04:14 -0300

    correçoes code climate

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 const name = document.getElementById('nome').value;
-const lastName = document.getElementById('sobrenome');
-const contact = document.getElementById('contato');
-const password = document.getElementById('senha');
-const date = document.getElementById('datepicker');
-const genderF = document.getElementById('feminino');
-const genderM = document.getElementById('masculino');
-const genderP = document.getElementById('personalizado');
-const btnCadastro = document.getElementById('facebook-register')
+const btnCadastro = document.getElementById('facebook-register');
 
 function validar() {
-  if (name.lenght == 0) {
+  if (name.lenght === 0) {
     window.alert('teste');
   } else {
     window.alert('ok');
@@ -18,4 +11,4 @@ function validar() {
 
 window.onload = function () {
   btnCadastro.addEventListener('click', validar);
-}
+};
