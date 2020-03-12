858b3e1cf51bdf98b5c0e1c593f374cd816ce6a7
Clayton Pereira <cpereiramt@gmail.com>
2020-03-12 18:46:24 -0400

    Alterando o formulario de cadastro

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 const buttonlogin = document.getElementById('button-login');
 buttonlogin.addEventListener('click', function () {
   const user = document.getElementById('user-email-phone').value;
-  alert('o usuário é : ' + user);
+  alert(user);
 });
