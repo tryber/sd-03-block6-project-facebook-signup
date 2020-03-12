92e8d6bd5b00e7853b77bff02aa5ae5de5e2e02d
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-12 18:49:58 -0300

    Removendo date picker por causa do code climate

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-document.getElementById('date').DatePickerX.init({ format: "dd/mm/yyyy" });
-
 document.getElementById('button-login').addEventListener('click', function () {
   alert(document.getElementById('user-email-phone').value);
   document.getElementById('user-email-phone').value = '';
