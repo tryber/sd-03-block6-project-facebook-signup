74fab6f782d6a724f8f659d0b2120d2916cab8cb
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-13 18:35:37 -0300

    adicionando date picker

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ botaoLogin.addEventListener('click', function () {
   const inputLoginValue = document.querySelector('#user-email-phone');
   alert(`${inputLoginValue.value}`);
 });
+
+/* eslint-env jquery*/
+$(function () {
+  $('#datepicker').datepicker({
+    dateFormat: 'dd/mm/aaaa'
+  });
+});
