f5ce8f8429dad322d4b5cf26ff2e4de967b64761
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-03-13 12:18:06 -0300

    ajustando o ultimo requisito

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const buttonlogin = document.querySelector('#button-login');
 
 buttonlogin.addEventListener('click', function () {
-  let user = document.getElementById('user-email-phone').value;
+  const user = document.getElementById('user-email-phone').value;
   alert(user);
 });
 
