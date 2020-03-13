f1d124ba74cdf865e60c9e9e8d0d2d14ded64326
Marco Barbosa <marco.meireles.b@gmail.com>
2020-03-13 11:35:16 -0300

    Atualizando o JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+const login = document.getElementById('button-login');
+const register = document.getElementById('facebook-register');
+
+login.addEventListener('click', function (){
+    const nomeEmail = document.getElementById('user-email-phone');
+    alert(nomeEmail.value);
+});
+
+register.addEventListener('click', function (){
+    const nomeCad = document.getElementById('nome');
+    const sobrenomeCad = document.getElementById('sobreNome');
+    const celularEmail = document.getElementById('celularEmail');
+    const dataNascimento = document.getElementById('data');
+    const genero = document.getElementsByClassName('genero');
+    alert(`${nomeCad.value}\n${sobrenomeCad.value}\n${celularEmail.value}\n${dataNascimento.value}\n${genero}`);
+});
