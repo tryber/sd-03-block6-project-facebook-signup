00f3839addc74e7e0b1ddd675c4f25a9e0b9a57e
Gabriel Oliveira Ferreira <gabriel1335555@gmail.com>
2020-03-14 16:41:50 -0300

    codeclimate#3

--- jquery-script.js
+++ jquery-script.js
@@ -1,25 +1,25 @@
-$(document).ready(function () {
-  $('#registration-form').validate({
-    rules: {
-      firstname: 'required',
-      lastname: 'required',
-      email: {
-        required: true,
-        email: true,
-        email1: { matches: '[0-9]+', minlength: 9, maxlength: 9 },
-      },
-      password: {
-        required: true,
-        minlength: 6,
-      },
-      bithdate: {
-        required: true,
-        date: true,
-      },
-    },
-  });
+// $(document).ready(function () {
+//   $('#registration-form').validate({
+//     rules: {
+//       firstname: 'required',
+//       lastname: 'required',
+//       email: {
+//         required: true,
+//         email: true,
+//         email1: { matches: '[0-9]+', minlength: 9, maxlength: 9 },
+//       },
+//       password: {
+//         required: true,
+//         minlength: 6,
+//       },
+//       bithdate: {
+//         required: true,
+//         date: true,
+//       },
+//     },
+//   });
 
-  $('#facebook-register').click({
+//   $('#facebook-register').click({
 
-  });
-});
+//   });
+// });
