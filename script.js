7bae6a6b7f5aa42e8d7e29092a96d06bca731195
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-14 17:20:06 -0300

    adiciona js

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const loginInput = document.getElementById("user-email-phone");
+const datePicker = document.querySelector('#datepicker').DatePickerX;
+datePicker.init({ format: 'dd/mm/yyyy' });
+datePicker.setValue('14/03/1995');
 
+const loginInput = document.querySelector('#user-email-phone');
+const loginButton = document.querySelector('#button-login');
+
+function loginAlert() {
+  alert(`Email ou telefone: ${loginInput.value}`);
+}
+
+loginButton.addEventListener('click', loginAlert);
+
+const radioButtons = document.querySelectorAll('.gender');
+const buttonCreate = document.querySelector('#facebook-register');
+
+function verificaDados() {
+  let firstName = document.querySelector('#firstname').value;
+  let selectGender = '';
+  for (let i = 0; i < radioButtons.length; i += 1) {
+    if (radioButtons[i].checked) {
+      selectGender = radioButtons[i].value;
+    }
+  }
+  alert(`${firstName} - ${selectGender}`);
+}
+
+buttonCreate.addEventListener('click', verificaDados);
