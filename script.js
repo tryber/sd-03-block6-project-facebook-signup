6f084248e0beceb23e61319922b599d35e72e5e0
wistyk <leonardoxw@gmail.com>
2020-03-15 19:16:49 -0300

    atualizacao codeclimate

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function cadastro() {
-    var nome = document.getElementById('nome').value
-    var sobrenome = document.getElementById('sobrenome').value
-    var celMail = document.getElementById('celularMail').value
-    var senha = document.getElementById('senha').value
-    var data = document.getElementById('data').value
+  let nome = document.getElementById('nome').value;
+  let sobrenome = document.getElementById('sobrenome').value;
+  let celMail = document.getElementById('celularMail').value;
+  let senha = document.getElementById('senha').value;
+  let data = document.getElementById('data').value;
 
-    if (nome.length, sobrenome.length, celMail.length, senha.length, data.length > 0) {
+  if (nome.length && sobrenome.length && celMail.length && senha.length && data.length > 0){
 
-        window.alert(`${nome} ${sobrenome} - ${celMail} - ${data}`)
-        
-    } else {
-        window.alert("Dados inválidos.")
-    }
+      window.alert(`${nome} ${sobrenome} - ${celMail} - ${data}`);      
+      }else{
+      window.alert('Dados inválidos.');
+  }
 }
