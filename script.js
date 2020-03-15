42559e2add76ac15c6f75adb73c4b1f9c4cfdcfc
Marylange <marylangess@gmail.com>
2020-03-15 16:42:52 -0300

    reparando erros do codeclimate 2

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ const pikaday = new Pikaday({
     weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
     weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
   },
-  toString(date, format) {
+  toString(date) {
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   },
-  parse(dateString, format) {
+  parse(dateString) {
     const parts = dateString.split('/');
     const day = parseInt(parts[0], 10);
     const month = parseInt(parts[1], 10) - 1;
     const year = parseInt(parts[2], 10);
     return new Date(year, month, day);
-  }
+  },
 });
 
-console.log(pikaday)
+console.log(pikaday);
