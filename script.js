5596deb0db23ef0450a48d13534ac735dab8a4c7
Marylange <marylangess@gmail.com>
2020-03-15 15:56:02 -0300

    reparando erros do codeclimate

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var picker = new Pikaday({
-  field: document.getElementById('datepicker'), i18n: {
+const picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  i18n: {
     previousMonth: 'Mês anterior',
     nextMonth: 'Próximo mês',
     months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
     weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-    weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
-  }
+    weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+  },
 });
