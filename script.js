68da06ce955f56b991985bad1dcdd26134006619
Marylange <marylangess@gmail.com>
2020-03-15 15:49:03 -0300

    acrescentando datepicket no trabalho em dupla

---
+++ script.js
@@ -0,0 +1,9 @@
+var picker = new Pikaday({
+  field: document.getElementById('datepicker'), i18n: {
+    previousMonth: 'Mês anterior',
+    nextMonth: 'Próximo mês',
+    months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
+  }
+});
