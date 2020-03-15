5e306a6cbcbcfa518e10d3a3c01ba80ccceb635c
Marylange <marylangess@gmail.com>
2020-03-15 16:37:38 -0300

    reparando erros do codeclimate 2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var pikaday = new Pikaday({
+const pikaday = new Pikaday({
   field: document.getElementById('datepicker'),
+  format: 'd/M/YYYY',
   i18n: {
     previousMonth: 'Mês anterior',
     nextMonth: 'Próximo mês',
@@ -7,6 +8,19 @@ var pikaday = new Pikaday({
     weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
     weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
   },
+  toString(date, format) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  }
 });
 
-pikaday = {};
+console.log(pikaday)
