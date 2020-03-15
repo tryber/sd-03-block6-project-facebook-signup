bcd2d0e22a4d0b0da23e43d134114de3da59026e
wscatao <wscatao@gmail.com>
2020-03-15 09:49:11 -0300

    Finalizado estilizacao da pagina, inserido lib externa para validacao de requisito para formatar input data de nascimento

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// libs
+window.DatePickerX.setDefaults(
+  {format:'dd/mm/yyyy',
+  weekDayLabels:['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom']}
+)
+
+document.getElementById('data').DatePickerX.init();
+
 // variaveis
 
 const buttonLogin = document.querySelector('#button-login');
 const userEmailPhone = document.querySelector('#user-email-phone');
+
 
 // funcoes
 
