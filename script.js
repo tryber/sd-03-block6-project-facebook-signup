c1741affd3a8190b6e517b9a8a1bc14abb6856fe
Gustavo <ggteodor07@gmai.com>
2020-03-16 18:25:05 -0300

    teste 6

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const buttonRegister = document.getElementById('facebook-register');
 buttonLogin.addEventListener('click', function () {
   const userEmailPhone = document.getElementById('user-email-phone').value;
   const userPassword = document.getElementById('user-password').value;
-  let frase = `User: ${userEmailPhone} Senha: ${userPassword}`;
-  alert(frase);
+  alert(`User: ${userEmailPhone} Senha: ${userPassword}`);
   document.getElementById('user-email-phone').value = '';
   document.getElementById('user-password').value = '';
 });
