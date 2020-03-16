54c0af2da37c55575a5b77351a454c927b7b68fd
William Igor <williamigorsilva@gmail.com>
2020-03-16 19:07:18 -0300

    Adicionando date

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-document.getElementById('date').DatePickerX.init();
-document.getElementById('date').DatePickerX.setValue(target.value,true);
+new Pikaday({ field: document.getElementById('date'), format:'DD/MM/YYYY', });
