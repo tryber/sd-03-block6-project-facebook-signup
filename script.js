208a72a1ba85467714bc45ab595530c608899e33
davidepadua <davidepadua1914@gmail.com>
2020-03-16 15:36:17 -0300

    vamos com tudo

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  const botaoEmailTelefone = document.getElementById('button-login');
+  botaoEmailTelefone.addEventListener('click', function () {
+    const emailTelefone = document.getElementById('user-email-phone');
+    window.alert(`Olá, ${emailTelefone.value}`);
+  });
+};
