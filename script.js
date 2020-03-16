3adb15d4579ebbdba13b0033492acaec20e8040e
tales <talestales@gmail.com>
2020-03-16 12:08:37 -0300

    ranquei tudo

--- script.js
+++ script.js
@@ -2,58 +2,58 @@
 
 window.DatePickerX.setDefaults(
   { format: 'dd/mm/yyyy',
-    weekDayLabels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom',] }
+    weekDayLabels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'] },
 );
 
 document.getElementById('data').DatePickerX.init();
 
 // validação do form
 
-new window.JustValidate('.form', {
-  rules: {
-    name: {
-      required: true,
-    },
-    password: {
-      required: true,
-      password: true,
-    },
-    sob: {
-      required: true,
-      minLength: 3,
-      maxLength: 30,
-    },
-    strength: {
-      // custom: '^([0-9]{9})|([A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3})$',
-      required: true,
-      minLength: 3,
-      maxLength: 30,
-    },
-    radio: {
-      required: true,
-    },
-  },
-  messages: {
-    name: {
-      required: 'Este campo é obrigatório',
-    },
-    password: {
-      required: 'Este campo é obrigatório',
-    },
-    sob: {
-      required: 'Este campo é obrigatório',
-    },
-    strength: {
-      required: 'Este campo é obrigatório',
-      // custom: 'Digite um e-mail ou telefone válido'
-    },
-    radio: {
-      required: 'Uma opção é obrigatória',
-    },
-  },
-  colorWrong: 'red',
-  focusWrongField: true,
-});
+// new window.JustValidate('.form', {
+//   rules: {
+//     name: {
+//       required: true,
+//     },
+//     password: {
+//       required: true,
+//       password: true,
+//     },
+//     sob: {
+//       required: true,
+//       minLength: 3,
+//       maxLength: 30,
+//     },
+//     strength: {
+//       // custom: '^([0-9]{9})|([A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3})$',
+//       required: true,
+//       minLength: 3,
+//       maxLength: 30,
+//     },
+//     radio: {
+//       required: true,
+//     },
+//   },
+//   messages: {
+//     name: {
+//       required: 'Este campo é obrigatório',
+//     },
+//     password: {
+//       required: 'Este campo é obrigatório',
+//     },
+//     sob: {
+//       required: 'Este campo é obrigatório',
+//     },
+//     strength: {
+//       required: 'Este campo é obrigatório',
+//       // custom: 'Digite um e-mail ou telefone válido'
+//     },
+//     radio: {
+//       required: 'Uma opção é obrigatória',
+//     },
+//   },
+//   colorWrong: 'red',
+//   focusWrongField: true,
+// });
 
 // variaveis
 
@@ -64,21 +64,21 @@ const form = document.querySelectorAll('.form input');
 
 // funcoes
 
-function funcaoAlert(event) {
-  event.preventDefault();
-  if (event.currentTarget.value === '1') {
-    alert(`${userEmailPhone.value}`);
-  } else {
-    let opt = document.getElementsByName('gender');
-    for (let i = 0; i < opt.length; i += 1) {
-      if (opt[i].checked) {
-        opt = opt[i].value;
-        console.log(opt);
-      }
-    }
-    alert(`${form[0].value} - ${opt}`);
-  }
-}
+// function funcaoAlert(event) {
+//   event.preventDefault();
+//   if (event.currentTarget.value === '1') {
+//     alert(`${userEmailPhone.value}`);
+//   } else {
+//     let opt = document.getElementsByName('gender');
+//     for (let i = 0; i < opt.length; i += 1) {
+//       if (opt[i].checked) {
+//         opt = opt[i].value;
+//         console.log(opt);
+//       }
+//     }
+//     alert(`${form[0].value} - ${opt}`);
+//   }
+// }
 
 // event listeners
 
