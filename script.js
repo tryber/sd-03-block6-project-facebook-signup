146b8df5f7b2eb718fb94e941667258619e74118
tales <talestales@gmail.com>
2020-03-16 12:02:01 -0300

    ?

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // libs
 
 window.DatePickerX.setDefaults(
-  {format:'dd/mm/yyyy',
-  weekDayLabels:['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom']}
-)
+  { format: 'dd/mm/yyyy',
+    weekDayLabels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom',] }
+);
 
 document.getElementById('data').DatePickerX.init();
 
 // validação do form
 
 new window.JustValidate('.form', {
-  rules:{
+  rules: {
     name: {
       required: true,
     },
@@ -27,37 +27,33 @@ new window.JustValidate('.form', {
       // custom: '^([0-9]{9})|([A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3})$',
       required: true,
       minLength: 3,
-      maxLength: 30
+      maxLength: 30,
     },
     radio: {
-      required: true
-    }
+      required: true,
+    },
   },
   messages: {
     name: {
-      required: 'Este campo é obrigatório'
+      required: 'Este campo é obrigatório',
     },
     password: {
-      required: 'Este campo é obrigatório'
+      required: 'Este campo é obrigatório',
     },
     sob: {
-      required: 'Este campo é obrigatório'
+      required: 'Este campo é obrigatório',
     },
-    strength : {
+    strength: {
       required: 'Este campo é obrigatório',
       // custom: 'Digite um e-mail ou telefone válido'
     },
-    radio : {
-      required: 'Uma opção é obrigatória'
-    }
+    radio: {
+      required: 'Uma opção é obrigatória',
+    },
   },
-  colorWrong: "red",
-  focusWrongField: true
+  colorWrong: 'red',
+  focusWrongField: true,
 });
-
-
-
-
 
 // variaveis
 
@@ -66,24 +62,22 @@ const userEmailPhone = document.querySelector('#user-email-phone');
 const buttonCreateAccount = document.querySelector('#facebook-register');
 const form = document.querySelectorAll('.form input');
 
-
 // funcoes
 
 function funcaoAlert(event) {
   event.preventDefault();
-  if (event.currentTarget.value === '1'){
+  if (event.currentTarget.value === '1') {
     alert(`${userEmailPhone.value}`);
   } else {
     let opt = document.getElementsByName('gender');
-    for (let i = 0; i < opt.length; i+=1) {
-      if(opt[i].checked) {
-        opt = opt[i].value
+    for (let i = 0; i < opt.length; i += 1) {
+      if (opt[i].checked) {
+        opt = opt[i].value;
         console.log(opt);
       }
     }
     alert(`${form[0].value} - ${opt}`);
   }
-  
 }
 
 // event listeners
