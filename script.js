c6cce4cd61f29f1afe4b17d51e0c1586bf8cc855
Gustavo <ggteodor07@gmai.com>
2020-03-16 17:34:08 -0300

    mais uma tentativa

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let buttonLogin = document.getElementById('button-login');
-let buttonRegister = document.getElementById('facebook-register');
+const buttonLogin = document.getElementById('button-login');
+const buttonRegister = document.getElementById('facebook-register');
 
-buttonLogin.addEventListener('click',function() {
-    let userEmailPhone = document.getElementById('user-email-phone').value;
-    let userPassword = document.getElementById('user-password').value;
-    let frase = 'User: '+userEmailPhone+' Senha: '+userPassword;
-    alert(frase);
-    document.getElementById('user-email-phone').value = '';
-    document.getElementById('user-password').value= '';
-})
+buttonLogin.addEventListener('click', function() {
+  const userEmailPhone = document.getElementById('user-email-phone').value;
+  const userPassword = document.getElementById('user-password').value;
+  const frase = `User: ${userEmailPhone} Senha: ${userPassword}`;
+  alert(frase);
+  document.getElementById('user-email-phone').value = '';
+  document.getElementById('user-password').value = '';
+});
 
-buttonRegister.addEventListener('click',function(){
-    
-})
+buttonRegister.addEventListener('click', function(){
+});
+
 
