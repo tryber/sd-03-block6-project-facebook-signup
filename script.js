9d7b903d4120b902d166c571d2957661eecc01c0
natox <alvares.renato@hotmail.com>
2020-03-17 15:36:46 -0300

    Getting datapicker right and form alert

--- script.js
+++ script.js
@@ -10,36 +10,26 @@ function alertLogin() {
 }
 buttonLogin.addEventListener('click', alertLogin);
 
-/* const facebookRegister = document.getElementById('facebook-register');
+const facebookRegister = document.getElementById('facebook-register');
 const inputNome = document.getElementById('input-nome');
 const inputSobrenome = document.getElementById('input-sobrenome');
 const inputCelularEmail = document.getElementById('input-celular-email');
 const inputSenha = document.getElementById('input-senha');
 const inputData = document.getElementById('input-data');
-const inputGenero = document.querySelector('radio').childNodes;
+const inputGenero = document.querySelector('radio');
 function alertRegister() {
-  const cadastro = document.querySelector('.cadastro > > input');
+  const cadastro = document.querySelector('.cadastro');
   let text = '';
   for (let i = 0; i < cadastro.length; i += 1) {
-         if (cadastro.elements[i].value === '') {
-          alert('Dados inválidos');
-      } else {
-    text += cadastro.elements[i].value + '<br>';
+    /*          if (cadastro.elements[i].value === '') {
+              alert('Dados inválidos');
+          } else { */
+
+    text = `${text} ${cadastro.elements[i].value} \n`
   }
   alert(text);
 }
 facebookRegister.addEventListener('click', alertRegister);
-
-$(function () {
-  $('#input-data').datepicker({
-    dateFormat: 'dd/mm/yy',
-    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'],
-    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
-    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'],
-    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-  });
-}); */
 
 const languages = document.querySelectorAll('.languages')[0];
 const footerOthers = document.querySelectorAll('.footerOthers')[0];
